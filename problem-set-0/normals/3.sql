fafb87f628db53520e3c16d930c4a14278a05145
Erik-Solveson <erik.solveson@gmail.com>
2025-02-24 11:22:05 -0700

    Finished assignments 1-6 out of 10 for problems 2/3 on week 0

---
+++ problem-set-0/normals/3.sql
@@ -0,0 +1,1 @@
+SELECT "0m", "100m", "200m", "longitude", "latitude" FROM "normals" WHERE  ("latitude" = '42.5' AND "longitude" = '-69.5')
---
+++ problem-set-0/normals/6.sql
@@ -0,0 +1,1 @@
+SELECT "latitude", "longitude", "50m" FROM "normals" WHERE ("latitude" < 20 AND "latitude" > 0) AND ("longitude" < 75 AND "longitude" > 55)
